d39a76612432bb9b606457d55f1f53e03475862d
Derek Kwasniewski <dsk42203@comcast.net>
2022-08-09 19:14:31 -0400

    Mass Changes: - Changed layout - Fixed code for when a row is selected but was never clicked on

--- MonthlyFinances.py
+++ MonthlyFinances.py
@@ -1,12 +1,11 @@
 # Monthly Finances
 # By Derek Kwasniewski
 
-from email import message
 import os
-from msilib.schema import ListBox
 from tkinter import *
 from tkinter import messagebox
 import PySimpleGUI as GUI
+import tkinter
 
 entriesList = []
 
@@ -21,7 +20,6 @@ if os.path.exists('payments.txt'):
             else:
                 list = line.split()
                 entriesList.append(list[0] + " " + list[1] + "\n")
-
 # Else create file if payments.txt file exists
 else:
     file = open("payments.txt", "w")
@@ -30,36 +28,61 @@ else:
 # Create GUI object using PySimpleGui Class
 GUI.theme("DarkAmber")
 layout = [
-    [GUI.Listbox(key="GUIList", values=entriesList, size=(0, 10), expand_x=True, )],
-    [GUI.Text("Enter Payment Name"), GUI.InputText(key="entryName"), GUI.InputText(key="entryCost")],
-    [GUI.Button("Ok", ), GUI.Button("Cancel")]
+    [GUI.Listbox(key="-LISTBOX-", values=entriesList, size=(20, 10))],
+    [GUI.Text("Name"), GUI.InputText(key="entryName", size=15)],
+    [GUI.Text("Cost"), GUI.InputText(key="entryCost", size=16)],
+    [GUI.Button("Ok"), GUI.Button("Cancel"), GUI.Button("Delete Element")]
 ]
 
-
 # Create the window
-window = GUI.Window("Window Title", layout)
+window = GUI.Window("Window Title", layout, finalize=True)
+listbox = window["-LISTBOX-"]
+widget = listbox.widget
+
+listbox.bind("<Button-1>", "", propagate=False)
+window.refresh()
+x0, y0, dx, dy = widget.bbox(GUI.tk.END)
+y_limit = y0 + dy
 
 def updateList(entryName, entryCost):
     entriesList.append(entryName + " " + entryCost + "\n")
     file = open("payments.txt", "a")
     file.write(entryName + " " + entryCost + "\n")
-    window["GUIList"].Update(values=entriesList)  # Lookup on pysimplegui.org the "find_element" function for the Window object (was updated to use a list lookup method)
+    window["-LISTBOX-"].update(values=entriesList)  # Lookup on pysimplegui.org the "find_element" function for the Window object (was updated to use a list lookup method)
+
 
 # Event loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     if event in(GUI.WIN_CLOSED, "Cancel"): # if user closes the window or clicks cancel
         break
+
     elif event == "Ok":
         nameText = values["entryName"]
         costText = values["entryCost"]
-
         if nameText == "":
             messagebox.showerror("Python Error", "Null Name!")
         elif costText == "":
             messagebox.showerror("Python Error!", "Null Cost!")
         else:
             updateList(nameText, costText)
+
+    elif event == "-LISTBOX-":
+        y = listbox.user_bind_event.y
+        if y  < y_limit:
+            index = widget.nearest(y)
+            listbox.update(set_to_index=index)
+        selectedElementIndex = window["-LISTBOX-"].get_indexes() # Get listbox element index as a list
+        print(selectedElementIndex)
+        # try:
+            # selectedElementIndex = selectedElementIndex[0] # Get the first index of list (The selected element index)
+        # except IndexError:
+            # continue
+        # del entriesList[selectedElementIndex]
+        # window["-LISTBOX-"].update(values=entriesList)
+
+    # elif event == "Delete Element":
+
 
 
 
